a37168575292dbe098071973586775ca72405721
Mane Harutyunyan <mena@Manes-Laptop.local>
2024-08-26 20:54:16 +0400

    update

--- August/generators/gen4.py
+++ August/generators/gen4.py
@@ -1,13 +1,13 @@
 # Use a generator expression to create a generator that yields the squares of numbers from 1 to 20.
 # Iterate through this generator to print all squared values.
 
-def generator():
-    for i in range(1,21):
-        result=i**2
-        yield result
-
-a=generator()
-for i in range(20):
+# def generator():
+#     for i in range(1,21):
+#         result=i**2
+#         yield result
+# a=generator()
+a=(x**2 for x in range(21))
+for i in range(21):
     print(next(a))
 
 
--- August/generators/gen8.py
+++ August/generators/gen8.py
@@ -1,13 +1,17 @@
 # Use a generator expression to filter and yield only even numbers from a list of numbers.
 #  Test this generator with a list of integers from 1 to 50.
 
-ls=[x for x in range(1,50)]
+ls=[x for x in range(1,51)]
 
-def generator(ls):
-    for i in range(len(ls)):
-        if i % 2 != 0:
-            yield i
+# def generator(ls):
+#     for i in range(len(ls)):
+#         if i % 2 != 0:
+#             yield i
 
-gen = generator(ls)
-for i in gen:
+# gen = generator(ls)
+# for i in gen:
+#     print(i)
+
+a = (x for x in ls if x%2==0)
+for i in a:
     print(i)
