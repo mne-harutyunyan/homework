a94915aaee42bb59fb1f086da6076f732c6d19db
mne-harutyunyan <har8yunyanman@gmail.com>
2025-01-12 23:36:19 +0400

    update

--- Network/FastAPI_Tasks/jwt token /model.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class User(BaseModel):
-    username:str = Field(..., min_length = 2)
-    password:str = Field(..., min_length = 2, max_length=50)
-    
-    
--- Network/library_management/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routers import users
-import uvicorn
-from app.helpers.config import settings
-
-app = FastAPI()
-app.include_router(users.router)
-if __name__ == '__main__':
-  uvicorn.run('main:app', port=settings.PORT, reload=True)
